608f5b83e0a1ab4bba4c9e5a43a5d95c9f591ad8
simonsjostrand-portfolio <simon.portfolio.git@gmail.com>
2025-05-26 16:47:13 +0200

    minor changes in rules/config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-export const INIT_FLICKS = 80;
+export const INIT_FLICKS = 100;
 export const MAX_FLICK_VALUE = 5;
 export const SLOT_TARGET_VALUE = 15;
 export const STARTING_LIFES = 3;
 export const POINTS_GAIN = 1;
 export const POINTS_LOSS = -1;
-export const POINTS_TO_WIN = 10;
+export const POINTS_TO_WIN = 14;
